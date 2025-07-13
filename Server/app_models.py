112f39501c48d44f468419e9c03e66a9b0f321df
yitzchakdamen <yitzchakdamen@gmail.com>
2025-07-13 10:12:22 +0300

    Refactor model info response structure and update API endpoint response model declaration

--- Server/app_models.py
+++ Server/app_models.py
@@ -4,11 +4,10 @@ from pydantic import BaseModel
 
 class ModelInfoResponse(BaseModel):
     name: Optional[str]
-    columns_all: List[Dict[str, List[str]]]
+    columns: List[str]
+    columns_all: Dict[str, List[str]]
     target_variable: Optional[List[str]]
-    columns:list
-    
+
 class PredictionRequest(BaseModel):
     model_name: str
     input_data: dict
-
